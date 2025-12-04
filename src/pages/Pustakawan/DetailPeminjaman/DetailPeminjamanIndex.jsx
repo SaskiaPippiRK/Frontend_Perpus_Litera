aa4f6289be512e3d18358f248f5dffda8797186b
SaskiaPippiRK <230712300@students.uajy.ac.id>
2025-12-05 00:04:14 +0700

    Update DetailPeminjamanIndex.jsx

--- src/pages/Pustakawan/DetailPeminjaman/DetailPeminjamanIndex.jsx
+++ src/pages/Pustakawan/DetailPeminjaman/DetailPeminjamanIndex.jsx
@@ -1,10 +1,5 @@
 import { useNavigate } from "react-router-dom";
-<<<<<<< Updated upstream
-import { useState, useEffect } from "react";
-import axios from 'axios';
-=======
 import axios from "axios";
->>>>>>> Stashed changes
 
 const API_BASE_URL = 'http://localhost:8000/api';
 
@@ -26,17 +21,6 @@ export default function DetailPeminjamanIndex() {
     };
 
     const handleDelete = async(id) => {
-<<<<<<< Updated upstream
-        if(window.confirm("Apakah Anda yakin ingin menghapus detail data peminjaman ini?")) {
-            try {
-                await axios.delete(`${API_BASE_URL}/detailPeminjaman/delete/${id}`);
-                alert("Detail peminjaman berhasil dihapus!");
-
-                fetchDetailPeminjaman();
-            } catch(err) {
-                console.error("Gagal menghapus detail data peminjaman: ", err);
-                alert("Gagal menghapus detail data peminjaman. Cek console untuk detail");
-=======
         if(window.confirm("Apakah Anda yakin ingin menghapus data peminjaman ini?")) {
             try {
                 await axios.delete(`${API_BASE_URL}/detailPeminjaman/delete/${id}`);
@@ -46,7 +30,6 @@ export default function DetailPeminjamanIndex() {
             } catch(err) {
                 console.error("Gagal menghapus detail peminjaman: ", err);
                 alert("Gagal menghapus detail peminjaman. Cek console untuk detail");
->>>>>>> Stashed changes
             }
         }
     };
@@ -55,36 +38,22 @@ export default function DetailPeminjamanIndex() {
     if(error) return <div className="p-4 alert alert-danger">{error}</div>;
 
     useEffect(() => {
-<<<<<<< Updated upstream
-        const fetchDetailPeminjaman = async () => {
-            try {
-                const response = await fetch(`${API_BASE_URL}/detailPeminjaman`, {
-=======
         const fetchPeminjaman = async () => {
             try {
                 const response = await fetch(`${API_BASE_URL}/peminjaman`, {
->>>>>>> Stashed changes
                     headers: {
                         "Authorization": "Bearer " + localStorage.getItem("token")
                     }
                 });
 
                 const data = await response.json();
-<<<<<<< Updated upstream
-                setAllDetailPeminjaman(data);
-=======
                 setAllPeminjaman(data);
->>>>>>> Stashed changes
             } catch (error) {
                 console.error(error);
             }
         };
 
-<<<<<<< Updated upstream
-        fetchDetailPeminjaman();
-=======
         fetchPeminjaman();
->>>>>>> Stashed changes
     }, []);
 
     return (
@@ -103,34 +72,16 @@ export default function DetailPeminjamanIndex() {
                                 <tr>
                                     <th className="text-center">ID</th>
                                     <th className="text-center">Nama Peminjam</th>
-<<<<<<< Updated upstream
-                                    <th className="text-center">Buku</th>
-                                    <th className="text-center">Status</th>
-                                    <th className="text-center">Jumlah</th>
-=======
                                     <th className="text-center">Judul Buku</th>
                                     <th className="text-center">Jumlah</th>
                                     <th className="text-center">Status</th>
->>>>>>> Stashed changes
                                     <th className="text-center">Denda</th>
                                     <th className="text-center">Aksi</th>
                                 </tr>
                             </thead>
                             <tbody>
                                 {allDetailPeminjaman.map((detailPeminjaman) => (
-                                    <tr key={detailPeminjaman.id_detailPeminjaman}>
-<<<<<<< Updated upstream
-                                        <td className="text-center">{detailPeminjaman.id_detailPeminjaman}</td>
-                                        <td className="text-center">{detailPeminjaman.peminjaman.user ? detailPeminjaman.peminjaman.user.nama : "-"}</td>
-                                        <td className="text-center">{detailPeminjaman.buku.judul}</td>
-                                        <td className="text-center">{detailPeminjaman.status}</td>
-                                        <td className="text-center">{detailPeminjaman.jumlah}</td>
-                                        <td className="text-center">{detailPeminjaman.denda}</td>
-
-                                        <td className="text-center">
-                                            <div style={{ display: "flex", justifyContent: "center", gap: "8px" }}>
-                                                <button className="btn btn-primary-edit" onClick={() => navigate(`/detailPeminjaman/edit/${detailPeminjaman.id_detailPeminjaman}`)}>
-=======
+                                    <tr key={detailPeminjaman.id_detail}>
                                         <td className="text-center">{detailPeminjaman.detailPeminjaman}</td>
                                         <td className="text-center">{detailPeminjaman.peminjaman.user ? detailPeminjaman.peminjaman.user.nama : "-"}</td>
                                         <td className="text-center">{detailPeminjaman.buku ? detailPeminjaman.buku.judul : "-"}</td>
@@ -139,11 +90,10 @@ export default function DetailPeminjamanIndex() {
                                         
                                         <td className="text-center">
                                             <div style={{ display: "flex", justifyContent: "center", gap: "8px" }}>
-                                                <button className="btn btn-primary-edit" onClick={() => navigate(`/peminjaman/edit/${detailPeminjaman.id_detailPeminjaman}`)}>
->>>>>>> Stashed changes
+                                                <button className="btn btn-primary-edit" onClick={() => navigate(`/peminjaman/edit/${detailPeminjaman.id_detail}`)}>
                                                     EDIT
                                                 </button>
-                                                <button className="btn btn-primary-danger" onClick={() => handleDelete(detailPeminjaman.id_detailPeminjaman)}>
+                                                <button className="btn btn-primary-danger" onClick={() => handleDelete(detailPeminjaman.id_detail)}>
                                                     HAPUS
                                                 </button>
                                             </div>
