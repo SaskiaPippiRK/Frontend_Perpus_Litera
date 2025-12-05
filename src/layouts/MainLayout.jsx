15bd66ef3fc78721fc0a1ab49077c6b607fa9882
deven-aditya <230712333@students.uajy.ac.id>
2025-12-05 11:28:58 +0700

    tambah isAuthenticated di mainlayout

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,7 +1,15 @@
 import Sidebar from "../components/Sidebar";
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 
 export default function MainLayout({ children }) {
+    const token = localStorage.getItem('auth_token');
+    const isAuthenticated = token && token.length > 5;
+
+    if(!isAuthenticated)
+    {
+        return <Navigate to="/login" replace />;
+    }
+
     return (
         <div className="layout-container">
             <Sidebar />
